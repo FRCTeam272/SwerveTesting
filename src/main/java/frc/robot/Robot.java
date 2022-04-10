bf72cce8858481c52ac5655f928730ee0e4d9b27
Jake Gadaleta <jake.gads@gmail.com>
2022-04-10 11:57:11 -0400

    init

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  
+  /** runs at zero packet */
+  @Override
+  public void robotInit() {
+    
+  }
+
+  /** runs every n+1 packet */
+  @Override
+  public void robotPeriodic() {
+    
+  }
+
+  /** First Disabled */
+  @Override
+  public void disabledInit() {}
+
+  /** every disabled */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** runs 1st auto packet/
+  @Override
+  public void autonomousInit() {
+    
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  /** runs first init */
+  @Override
+  public void teleopInit() {
+    
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    
+  }
+
+  @Override
+  public void testPeriodic() {}
+}
