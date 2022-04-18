bc4357a5f861db7abc8da526c64c3b48991b3ef5
Jake Gadaleta <Me@jakegads.dev>
2022-04-18 19:26:35 -0400

    added the 103 imp as a text file

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -29,5 +29,20 @@ public class Drive extends SubsystemBase {
     // TODO: Find this value
     public static final double WHEEL_DIAMETER = 4.0;
 
+    // TODO: increase MAX_SPEED
+	public static final double MAX_SPEED = 0.3; // Max speed is 0 to 1
+	public static final double STEER_DEGREES_PER_COUNT = 360.0 / ENCODER_COUNT_PER_ROTATION;
+    // Jake - I literally have no idea what this math is or where these values are coming from but I aint gonna touch it
+	public static final double DRIVE_INCHES_PER_COUNT = (WHEEL_DIAMETER * Math.PI) / (80.0 * 6.67); 
+	public static final double DEADZONE = 0.1;
+	public static final double MAX_REVERSIBLE_SPEED_DIFFERENCE = 0.5 * MAX_SPEED;
+
+    private static final double STEER_P = .2, STEER_I = 0.00, STEER_D = 0.002;
+    private static final double RAMP_RATE = 0.5;
+    public static final double OMEGA_SCALE = 1.0 / 30.0;
+    private double originHeading = 0.0;
+    private final double leftPow = 1.0;
+	private final double rightPow = 1.0;
+
 
 }
